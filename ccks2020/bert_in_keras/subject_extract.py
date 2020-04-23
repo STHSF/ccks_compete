6208803ee48fc0f1fc9131824b813c42ec490152
li <liyu52577@163.com>
2020-04-23 13:24:49 +0800

    update code

--- ccks2020/bert_in_keras/subject_extract.py
+++ ccks2020/bert_in_keras/subject_extract.py
@@ -303,7 +303,7 @@ def test():
     F = open('result.txt', 'w')
     for d in tqdm(iter(test_data)):
         _object, _category = extract_entity(d[1])
-        s = '%s\t%s\t%s\n' % (d[0], _category, _object)
+        s = '%s\t%s\t%s\t%s\n' % (d[0], d[1], _category, _object)
         s = s.encode('utf-8')
         F.write(str(s))
     F.close()
