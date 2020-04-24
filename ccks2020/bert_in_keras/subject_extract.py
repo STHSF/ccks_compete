0c846d34a23daa2fe80b6d432c2c76240673039c
li <liyu52577@163.com>
2020-04-24 18:48:44 +0800

    code update

--- ccks2020/bert_in_keras/subject_extract.py
+++ ccks2020/bert_in_keras/subject_extract.py
@@ -353,9 +353,7 @@ train_D = data_generator(train_data)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--is_train', dest='is_train',
-                        default=True,
-                        type=bool, help="train or test")
+    parser.add_argument('--is_train', dest='is_train', default=False, type=bool, help="train or test")
     args = parser.parse_args()
     is_train = args.is_train
     print(is_train)
